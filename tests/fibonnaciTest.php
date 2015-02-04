944fbca50aed96c120cbc52d580a4aa221822322
ajace <ace.atienza@about.me>
2015-02-03 23:38:08 -0500

    add more tests

--- tests/fibonnaciTest.php
+++ tests/fibonnaciTest.php
@@ -3,10 +3,33 @@
 require_once '../fibonacci.php';
 
 class FibonacciTest extends PHPUnit_Framework_TestCase {
-    public function testFibonacciTen() {
+
+    // TODO: mock the Fibonacci object
+    
+    protected $fib;
+
+    public function setUp() 
+    {
+        $this->fib = new Fibonacci(10);
+    }
+
+    public function testPrep() 
+    {
+        $case = $this->fib->prep(10);
+
+        $this->assertInternalType("int", $case);
+    }
+
+    public function testFibonacciTen() 
+    {
         $first10 = array(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55);
-        $fib = new Fibonacci(10);
-        
-        $this->assertEquals($first10, $fib->numbers);
-    }    
+
+        $this->assertEquals($first10, $this->fib->numbers);
+    }
+
+    public function testGenerateOutputIsString() 
+    {
+        $case = $this->fib->generate_output();
+        $this->assertContains("Execution time", $case);
+    }
 }
