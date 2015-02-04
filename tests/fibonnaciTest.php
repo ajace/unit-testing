dc4171ce7f0200a778a09485885e4a4e5aa9891f
ajace <ace.atienza@about.me>
2015-02-03 23:22:46 -0500

    cleanup

--- tests/fibonnaciTest.php
+++ tests/fibonnaciTest.php
@@ -6,8 +6,7 @@ class FibonacciTest extends PHPUnit_Framework_TestCase {
     public function testFibonacciTen() {
         $first10 = array(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55);
         $fib = new Fibonacci(10);
-        // var_dump($fib->numbers);
-        // $actual = $fib->calculate();
+        
         $this->assertEquals($first10, $fib->numbers);
     }    
 }
